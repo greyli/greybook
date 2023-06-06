233bac5c2ccb8be01d55174aa433c9cd35556f55
Grey Li <withlihui@gmail.com>
2023-06-06 21:32:31 +0800

    Migrate from Flask-Mail to Flask-Mailman

--- bluelog/emails.py
+++ bluelog/emails.py
@@ -1,19 +1,18 @@
 from threading import Thread
 
 from flask import url_for, current_app
-from flask_mail import Message
-
-from bluelog.extensions import mail
+from flask_mailman import EmailMessage
 
 
 def _send_async_mail(app, message):
     with app.app_context():
-        mail.send(message)
+        message.send()
 
 
-def send_mail(subject, to, html):
+def send_mail(subject, body, to):
     app = current_app._get_current_object()
-    message = Message(subject, recipients=[to], html=html)
+    message = EmailMessage(subject, body=body, to=[to])
+    message.content_subtype = 'html'
     thr = Thread(target=_send_async_mail, args=[app, message])
     thr.start()
     return thr
@@ -21,17 +20,23 @@ def send_mail(subject, to, html):
 
 def send_new_comment_email(post):
     post_url = url_for('blog.show_post', post_id=post.id, _external=True) + '#comments'
-    send_mail(subject='New comment', to=current_app.config['BLUELOG_EMAIL'],
-              html='<p>New comment in post <i>%s</i>, click the link below to check:</p>'
-                   '<p><a href="%s">%s</a></P>'
-                   '<p><small style="color: #868e96">Do not reply this email.</small></p>'
-                   % (post.title, post_url, post_url))
+    send_mail(
+        subject='New comment',
+        body='<p>New comment in post <i>%s</i>, click the link below to check:</p>'
+             '<p><a href="%s">%s</a></P>'
+             '<p><small style="color: #868e96">Do not reply this email.</small></p>'
+             % (post.title, post_url, post_url),
+        to=current_app.config['BLUELOG_ADMIN_EMAIL']
+    )
 
 
 def send_new_reply_email(comment):
     post_url = url_for('blog.show_post', post_id=comment.post_id, _external=True) + '#comments'
-    send_mail(subject='New reply', to=comment.email,
-              html='<p>New reply for the comment you left in post <i>%s</i>, click the link below to check: </p>'
-                   '<p><a href="%s">%s</a></p>'
-                   '<p><small style="color: #868e96">Do not reply this email.</small></p>'
-                   % (comment.post.title, post_url, post_url))
+    send_mail(
+        subject='New reply',
+        body='<p>New reply for the comment you left in post <i>%s</i>, click the link below to check: </p>'
+             '<p><a href="%s">%s</a></p>'
+             '<p><small style="color: #868e96">Do not reply this email.</small></p>'
+            % (comment.post.title, post_url, post_url),
+        to=comment.email
+    )
--- bluelog/settings.py
+++ bluelog/settings.py
@@ -22,13 +22,13 @@ class BaseConfig(object):
     CKEDITOR_FILE_UPLOADER = 'admin.upload_image'
 
     MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = ('Bluelog Admin', MAIL_USERNAME)
+    MAIL_DEFAULT_SENDER = f'Bluelog <{MAIL_USERNAME}>'
 
-    BLUELOG_EMAIL = os.getenv('BLUELOG_EMAIL')
+    BLUELOG_ADMIN_EMAIL = os.getenv('BLUELOG_ADMIN_EMAIL')
     BLUELOG_POST_PER_PAGE = 10
     BLUELOG_MANAGE_POST_PER_PAGE = 15
     BLUELOG_COMMENT_PER_PAGE = 15
